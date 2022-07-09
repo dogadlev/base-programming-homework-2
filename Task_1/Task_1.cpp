a58b193f46a32ea145f05f9f138521f81c33a2d7
Andrei Dogadaev <dogadlev@yandex.ru>
2022-07-09 16:58:56 +0300

    Change Task_1.cpp.

--- Task_1/Task_1.cpp
+++ Task_1/Task_1.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-enum yMonths
+enum class yMonths
 {
     Jan = 1,
     Feb,
@@ -18,78 +18,89 @@ enum yMonths
     Dec
 };
 
+void printMonth(int month) 
+{
+    switch (static_cast<yMonths>(month))
+    {
+    case yMonths::Jan:
+        std::cout << "Январь" << std::endl;
+        break;
+
+    case yMonths::Feb:
+        std::cout << "Февраль" << std::endl;
+        break;
+
+    case yMonths::Mar:
+        std::cout << "Март" << std::endl;
+        break;
+
+    case yMonths::Apr:
+        std::cout << "Апрель" << std::endl;
+        break;
+
+    case yMonths::May:
+        std::cout << "Май" << std::endl;
+        break;
+
+    case yMonths::Jun:
+        std::cout << "Июнь" << std::endl;
+        break;
+
+    case yMonths::Jul:
+        std::cout << "Июль" << std::endl;
+        break;
+
+    case yMonths::Aug:
+        std::cout << "Август" << std::endl;
+        break;
+
+    case yMonths::Sep:
+        std::cout << "Сентябрь" << std::endl;
+        break;
+
+    case yMonths::Oct:
+        std::cout << "Октябрь" << std::endl;
+        break;
+
+    case yMonths::Nov:
+        std::cout << "Ноябрь" << std::endl;
+        break;
+
+    case yMonths::Dec:
+        std::cout << "Декабрь" << std::endl;
+        break;
+
+    default:
+        break;
+    }
+}
+
 int main(int argc, char** argv)
 {
     setlocale(LC_ALL, "Russian");
 
-    int month = 0;
-    bool q = true;
+    int month = 1;
+    bool q = true; //Bool variable to quit from the loop below.
 
     while (q)
     {
-        std::cout << "Введите номер месяца: ";
+        std::cout << "Введите номер месяца (целое число от 1 до 12): ";
         std::cin >> month;
-
-        switch (month)
+        
+        if (month >= 1 && month <= 12)
         {
-        case(0):
+            printMonth(month);
+        }
+        else if (month == 0)
+        {
             std::cout << "До свидания" << std::endl;
             q = false;
-            break;
-
-        case(yMonths::Jan):
-            std::cout << "Январь" << std::endl;
-            break;
-
-        case(yMonths::Feb):
-            std::cout << "Февраль" << std::endl;
-            break;
-
-        case(yMonths::Mar):
-            std::cout << "Март" << std::endl;
-            break;
-
-        case(yMonths::Apr):
-            std::cout << "Апрель" << std::endl;
-            break;
-
-        case(yMonths::May):
-            std::cout << "Май" << std::endl;
-            break;
-
-        case(yMonths::Jun):
-            std::cout << "Июнь" << std::endl;
-            break;
-
-        case(yMonths::Jul):
-            std::cout << "Июль" << std::endl;
-            break;
-
-        case(yMonths::Aug):
-            std::cout << "Август" << std::endl;
-            break;
-
-        case(yMonths::Sep):
-            std::cout << "Сентябрь" << std::endl;
-            break;
-
-        case(yMonths::Oct):
-            std::cout << "Октябрь" << std::endl;
-            break;
-
-        case(yMonths::Nov):
-            std::cout << "Ноябрь" << std::endl;
-            break;
-
-        case(yMonths::Dec):
-            std::cout << "Декабрь" << std::endl;
-            break;
-
-        default:
-            std::cout << "Вы ввели неверный номер месяца. Попробуйте целое числа от 1 до 12." << std::endl;
-            break;
+        }
+        else
+        {
+            std::cout << "Вы ввели неправильный номер месяца. Попробуйте целое число от 1 до 12." << std::endl;
         }
     }
-
+    
     return 0;
 }
